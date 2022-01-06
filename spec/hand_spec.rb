05eee4e8458a8cc7fb252e8b8b0e31e24530bb83
nurbu5 <dsherpa@DESKTOP-309T1MV.localdomain>
2022-01-05 20:54:16 -0700

    Add unit specs for the hand class

---
+++ spec/hand_spec.rb
@@ -0,0 +1,43 @@
+require_relative '../hand.rb'
+
+describe Hand do
+  describe '#description' do
+    it 'should properly describe a hand with two cards' do
+      hand = Hand.new
+
+      hand.add_card({suit: "hearts", rank: 10})
+      hand.add_card({suit: "spades", rank: 2})
+
+      expect(hand.description).to eq("10 of hearts, 2 of spades")
+    end
+
+    it 'should properly describe a hand containing an Ace and face card' do
+      hand = Hand.new
+
+      hand.add_card({suit: "diamonds", rank: 1})
+      hand.add_card({suit: "clubs", rank: 13})
+
+      expect(hand.description).to eq("Ace of diamonds, King of clubs")
+    end
+  end
+
+  describe '#score' do
+    it 'should properly calculate the score of a hand with two cards' do
+      hand = Hand.new
+
+      hand.add_card({suit: "hearts", rank: 10})
+      hand.add_card({suit: "spades", rank: 2})
+
+      expect(hand.score).to eq(12)
+    end
+
+    it 'should properly calculate the score of a hand containing an Ace and a face card' do
+      hand = Hand.new
+
+      hand.add_card({suit: "diamonds", rank: 1})
+      hand.add_card({suit: "clubs", rank: 13})
+
+      expect(hand.score).to eq(14)
+    end
+  end
+end
